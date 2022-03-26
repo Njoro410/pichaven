6ac43aa553c5b599c79db757b0ddc9f89c54c9c5
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-26 10:46:23 +0300

    Models: added image model

--- favpics/models.py
+++ favpics/models.py
@@ -1,3 +1,9 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='/pics', default='Image')
