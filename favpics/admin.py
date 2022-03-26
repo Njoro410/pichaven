fe3d9e159921f55c6c4073c49c3eb1af5d04a5e9
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-26 17:23:45 +0300

    Admin: added location model to admin

--- favpics/admin.py
+++ favpics/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Image,Location,Category
-# Register your models here.
+from .models import Image,Category,Location
 
 admin.site.register(Image)
 admin.site.register(Location)
