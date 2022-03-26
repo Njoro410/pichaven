a6d7c08fccaeb5196941769bf51922fa3bef8970
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-26 12:09:06 +0300

    Migrations: made new migrations

--- favpics/models.py
+++ favpics/models.py
@@ -1,20 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+        
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
 
 class Image(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     images = models.ImageField(upload_to='pics/', default='Image')
-    cat = models.ForeignKey(Category, on_delete=CASCADE)
-    location = models.ForeignKey(Location, on_delete=CASCADE)
-    
-    
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    
-    
-class Location(models.Model):
-    name = models.CharField(max_length=50)
+
+
+
+
